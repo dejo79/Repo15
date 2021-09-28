718e4430a36283a9f4f293af9a8e7ce381e42eec
dejo79 <dstefatic@gmail.com>
2021-09-28 09:13:52 +0200

    Added new files

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,8 +1,10 @@
 const milili = [10, 25, 4];
 function myArr(mili) {
     'use strict';
-
+    mili[0];
+    mili[1];
+    mili[2];
     return mili;
 }
-console.log(myArr());
+console.log(myArr(milili));
 module.exports = myArr;
