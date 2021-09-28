bd026b4af90c14c92327f069b60f1361281add7b
dejo79 <dstefatic@gmail.com>
2021-09-28 09:43:58 +0200

    Added new files

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,4 +1,4 @@
-const myArray = (array1, array2) => array1 * array2;
+const myArray = (array1, array2) => array1.concat(array2);
 "use strict";
 return array1.concat(array2);
 console.log(myArray([10, 11], [12, 13, 14, 15]));
